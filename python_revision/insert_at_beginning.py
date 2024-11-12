740e2ef59b545f0b87ca3160b392be73e7621135
Srushti Samant <74961958+srushtisamant@users.noreply.github.com>
2024-11-12 08:38:59 +0000

    implemented insert at beginning function

---
+++ python_revision/insert_at_beginning.py
@@ -0,0 +1,47 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+    
+class linked_list:
+    def __init__(self):
+        self.head=None
+    
+    def append_at_end(self,data):
+        new_node=node(data)
+        if self.head is None:
+            self.head=new_node
+            new_node=self.head
+            return
+        
+        curr=self.head
+        while curr.next:
+            curr=curr.next
+        
+        curr.next=new_node
+
+    def print_linked_list(self):
+        if self.head is None:
+            print("empty list")
+        
+        curr=self.head
+        while curr:
+            print(curr.data,end="->")
+            curr=curr.next
+
+        print("None")
+
+    def insert_at_beginning(self,data):
+        new_node=node(data)
+        new_node.next=self.head
+        self.head=new_node
+
+
+
+
+ll=linked_list()
+ll.append_at_end(1)
+ll.append_at_end(2)
+ll.append_at_end(3)
+ll.insert_at_beginning(0)
+ll.print_linked_list()
